bb7411e24bf5eac2fd665fae095dee40b0a618aa
Yihong Wang <yh.wang@ibm.com>
2018-07-09 18:12:25 -0700

    Add a mic button
    Use the microphone button to start the recording
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- Tenssketch.js
+++ Tenssketch.js
@@ -12,60 +12,56 @@ var playflag=false;
 //global objects
 var myHeading = document.querySelector('h1');
 var recorder = new p5.SoundRecorder(); // recorder object
-var soundFile = new p5.SoundFile(); // creates a soundfile to save the recorded audio
+var soundFile;
+var mic;
 
 
 
 function setup() // always executes first
 {
-	//myHeading.textContent = 'Recording!';
-	Noise=0;
+  //myHeading.textContent = 'Recording!';
+  Noise=0;
 
-	//tf.tensor([1, 2, 3, 4]).print(); //just testing
-	//tf.tensor2d([1, 2, 3, 5], [2, 2]).print(); //just testing
+  //tf.tensor([1, 2, 3, 4]).print(); //just testing
+  //tf.tensor2d([1, 2, 3, 5], [2, 2]).print(); //just testing
 
-
-	mic = new p5.AudioIn();
+  mic = new p5.AudioIn();
   mic.start();
-	i=0;
-	sum=0;
-	recorder.setInput(mic);
+  i=0;
+  sum=0;
+  recorder.setInput(mic);
+}
+
+function startRec()
+{
+  myHeading = document.querySelector('h1');
+  micButton = document.getElementById('mic');
+  micButton.disabled = true;
+  soundFile = new p5.SoundFile(); // creates a soundfile to save the recorded audio
+  background(0);
+
+  recorder.record(soundFile);
+  myHeading.textContent= 'Recording!';
+  setTimeout(stopRec, 2000);
+  playflag = true;
 }
 
 function draw() //loops infinitely
 {
+  if (!playflag) return;
 
-	myHeading = document.querySelector('h1');
-	background(0);
+  background(0);
   micLevel = mic.getLevel();
-
-	pushVals(micLevel);
-	Noise= avgLevel;
-
-	if(i<999)
-	{
-	//tf.scalar(micLevel).print(); //prints the amplitude values of the audio samples
-	//tf.scalar(Noise).print();
-	i++;
-	}
-
-	if(micLevel>= (Noise+0.03)) //start recording if
-	{
-		recorder.record(soundFile);
-		myHeading.textContent= 'Recording!';
-		setTimeout(StopRec, 2000);
-	}
-ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
-
-if(playflag==true)
-{
-		recorder.stop();
-		soundFile.play();
+  pushVals(micLevel);
+  Noise= avgLevel;
+  if(i<999)
+  {
+  //tf.scalar(micLevel).print(); //prints the amplitude values of the audio samples
+  //tf.scalar(Noise).print();
+    i++;
+  }
+  ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
 }
-
-
-}
-
 
 function scrollWindow(x, y) //function used to scroll through the page
 {
@@ -74,52 +70,52 @@ function scrollWindow(x, y) //function used to scroll through the page
 
 function pushVals(val)
 {
-	 sum+=val;
+  sum+=val;
 
-   levels[ind] = val;
-	 //tf.tensor(levels).print();
+  levels[ind] = val;
+  //tf.tensor(levels).print();
 
+  if(ind != 99) //setting value of push index
+  {
+    ind++;
+  }
+  else
+  {
+    ind = 0;
+  }
 
-		if(ind != 99) //setting value of push index
-		{
-			ind++;
-		}
-		else
-		{
-			ind = 0;
-		}
+  if(levels.length>=100) //if queue is full
+  {
+    offset=levels[offsetIndex];
+    sum-=offset;
+    if(offsetIndex==99) //setting offset Values to aid mean calculation
+    {
+      offsetIndex=0;
+    }
+    else
+    {
+      offsetIndex++;
+    }
+  }
 
-
-
-	 if(levels.length>=100) //if queue is full
-	 {
-		 		offset=levels[offsetIndex];
-				sum-=offset;
-
-				if(offsetIndex==99) //setting offset Values to aid mean calculation
-				{
-						offsetIndex=0;
-				}
-				else
-				{
-						offsetIndex++;
-				}
- 		}
-
-	 avgLevel=sum/levels.length;
-
+  avgLevel=sum/levels.length;
 }
 
 //funstion to stop recording audio file and play it back
-function StopRec()
+function stopRec()
 {
-	playflag=true;
-
+  myHeading = document.querySelector('h1');
+  mmicButtonic = document.getElementById('mic');
+  micButton.disabled = false;
+  myHeading.textContent= 'Not Recording!';
+  recorder.stop();
+  soundFile.play();
+  playflag = false;
 }
 
 function show(paramz)
 {
-	tf.tensor(paramz).print();
+  tf.tensor(paramz).print();
 }
 
 
